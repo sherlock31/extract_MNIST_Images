a39570c7c9b4f5d61a4ae188170d2a9bc9b15375
sherlock31 <balrajholmes@gmail.com>
2018-10-11 02:37:15 +0530

    second commit

--- extract_mnist.py
+++ extract_mnist.py
@@ -6,16 +6,16 @@ from PIL import Image
 for i in range(0,10000):
     temp = x_test[i]
 
-    path = "/content/drive/My Drive/MNIST_extract/stdp_extract_mycode/test/" + str(y_test[i]) + '/'
-    
+    #path = "/content/drive/My Drive/MNIST_extract/stdp_extract_mycode/test/" + str(y_test[i]) + '/'
+    path = "test/" + str(y_test[i]) + '/'
     img_temp = Image.fromarray(temp)
     img_temp.save(path + str(i) + '.jpg')
 
 for i in range(0,59999):
     temp = x_train[i]
 
-    path = "/content/drive/My Drive/MNIST_extract/stdp_extract_mycode/train/" + str(y_train[i]) + '/'
-    
+    #path = "/content/drive/My Drive/MNIST_extract/stdp_extract_mycode/train/" + str(y_train[i]) + '/'
+    path = "train/" + str(y_train[i]) + '/'
     img_temp = Image.fromarray(temp)
     img_temp.save(path + str(i) + '.jpg')
 
